273c3687b5b261668e8dc9aa4eb8be0229c6750e
Felix Cuadrilla <felix.cuadrilla@gmail.com>
2023-06-04 14:06:27 -0700

    Algorithms

--- Algorithms/AlgorithmTest.cs
+++ Algorithms/AlgorithmTest.cs
@@ -1,18 +1,27 @@
+using System;
 using Xunit;
 
 namespace DeveloperSample.Algorithms
 {
     public class AlgorithmTest
     {
-        [Fact(Skip="Not implemented")]
+        [Fact]
         public void CanGetFactorial()
         {
+            Assert.Equal(1, Algorithms.GetFactorial(0));
+            Assert.Equal(1, Algorithms.GetFactorial(1));
             Assert.Equal(24, Algorithms.GetFactorial(4));
+            Assert.Equal(120, Algorithms.GetFactorial(5));
+            Assert.Throws<ArgumentOutOfRangeException>(() => Algorithms.GetFactorial(-1));
         }
 
-        [Fact(Skip="Not implemented")]
+        [Fact]
         public void CanFormatSeparators()
         {
+            Assert.Equal("", Algorithms.FormatSeparators());
+            Assert.Equal("", Algorithms.FormatSeparators(null));
+            Assert.Equal("a", Algorithms.FormatSeparators("a"));
+            Assert.Equal("a and b", Algorithms.FormatSeparators("a", "b"));
             Assert.Equal("a, b and c", Algorithms.FormatSeparators("a", "b", "c"));
         }
     }
--- Algorithms/Algorithms.cs
+++ Algorithms/Algorithms.cs
@@ -1,11 +1,37 @@
 using System;
+using System.Text;
 
 namespace DeveloperSample.Algorithms
 {
     public static class Algorithms
     {
-        public static int GetFactorial(int n) => throw new NotImplementedException();
+        public static int GetFactorial(int n)
+        {
+            if (n < 0)
+                throw new ArgumentOutOfRangeException(nameof(n));
+            int tot = 1;
+            for (int i = 2; i <= n; i++)
+                tot *= i;
+            return tot;
+        }
 
-        public static string FormatSeparators(params string[] items) => throw new NotImplementedException();
+        public static string FormatSeparators(params string[] items)
+        {
+            if (items == null || items.Length == 0)
+                return "";
+            int limit = items.Length - 1;
+            int commaLimit = limit - 1;
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < limit; i++)
+            {
+                sb.Append(items[i]);
+                if (i < commaLimit)
+                    sb.Append(", ");
+                else
+                    sb.Append(" and ");
+            }
+            sb.Append(items[limit]);
+            return sb.ToString();
+        }
     }
 }
